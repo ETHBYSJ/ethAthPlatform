5b1d4331562d6925666bf2286098a8cb121725a5
ShuiShuiMeng <981904597@qq.com>
2018-04-20 14:25:13 +0800

    添加mongodb

---
+++ mongodb/bcindex.js
@@ -0,0 +1,14 @@
+/**
+ * Collections: dbindex
+ */
+
+var mongoose = require('./db.js');
+
+var bcindexSchema = new mongoose.Schema({
+    id:    { type: Number },
+    time:  { type: Date },
+    index: { type: Number },
+    extra: { type: String }   
+});
+
+module.exports = mongoose.model('bcindex', bcindexSchema)
---
+++ mongodb/db.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+//model/config.js内部有mongodb的连接属性
+var config = require('./config.js');
+
+//连接mongodb
+mongoose.connect(config.mongodb);
+
+//检测连接情况
+/**
+ * 连接成功
+ */
+mongoose.connection.on("open", function() {
+    console.log("Mongoose connection succeeded : " + config.mongodb);
+});
+
+/**
+ * 连接异常
+ */
+mongoose.connection.on("error", function() {
+    console.log("Mongoose connection failed:" + error);
+});
+
+/**
+ * 连接断开
+ */
+mongoose.connection.on("disconnect", function() {
+    console.log("Mongoose connection disconnected");
+})
+
+module.exports = mongoose;
